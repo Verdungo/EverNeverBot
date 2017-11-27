3b9977fc7142360bd7c411963bd7c4bbc94ae552
Verdungo <myxomop.k66@gmail.com>
2017-11-28 00:17:06 +0500

    no message

--- src/Bot/NeverEverBot.java
+++ src/Bot/NeverEverBot.java
@@ -1,12 +1,13 @@
 package Bot;
 
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Chat;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.User;
+import org.telegram.telegrambots.bots.AbsSender;
 import org.telegram.telegrambots.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
-import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 public class NeverEverBot extends TelegramLongPollingCommandBot {
 
@@ -15,6 +16,7 @@ public class NeverEverBot extends TelegramLongPollingCommandBot {
 
     public NeverEverBot() {
         super(botUsername);
+        registerCommands();
     }
 
     @Override
@@ -35,15 +37,28 @@ public class NeverEverBot extends TelegramLongPollingCommandBot {
         return botToken;
     }
 
-    public static void main(String[] args) {
-        ApiContextInitializer.init();
+    private void registerCommands() {
+        BotCommand newQuestionCommand = new BotCommand("never","Я никогда не...") {
+            @Override
+            public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
+                StringBuilder sb = new StringBuilder();
+                for (String s :
+                        arguments) {
+                    sb.append(s).append(" ");
+                }
 
-        TelegramBotsApi botsApi = new TelegramBotsApi();
+                SendMessage msg = new SendMessage()
+                        .setText("Новый вопрос - \""+ sb.toString().trim() +"\"")
+                        .setChatId(chat.getId());
 
-        try {
-            botsApi.registerBot(new NeverEverBot());
-        } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
-        }
+                try {
+                    absSender.execute(msg);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        register(newQuestionCommand);
     }
+
 }
--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,8 +1,30 @@
 package Game;
 
+import Bot.NeverEverBot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
 public class Game {
     private boolean active = false;
     private String question = "";
+    private static NeverEverBot bot;
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        bot = new NeverEverBot();
+
+        try {
+            botsApi.registerBot(bot);
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     public void startGame(String q) {
         question = q;
