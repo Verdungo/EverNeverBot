3f24164cf0fd4e3709de05d4d006ae4137d6126b
Verdungo <myxomop.k66@gmail.com>
2017-11-30 23:28:39 +0500

    no message

--- src/Bot/NeverEverBot.java
+++ src/Bot/NeverEverBot.java
@@ -1,34 +1,102 @@
 package Bot;
 
+import Game.Game;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Chat;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.api.objects.User;
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.bots.AbsSender;
 import org.telegram.telegrambots.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class NeverEverBot extends TelegramLongPollingCommandBot {
 
     private static final String botToken = "462721270:AAHLtx9MXbeYivPP_SyQ9Sg6GeX3nMEsm54";
     private static final String botUsername = "EverNeverBot";
+    private Map<Long,Game> games = new HashMap<>();
 
     public NeverEverBot() {
         super(botUsername);
-        registerCommands();
+        // не нужна команда
+        //registerCommands();
+    }
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        TelegramLongPollingCommandBot bot = new NeverEverBot();
+
+        try {
+            botsApi.registerBot(bot);
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void processNonCommandUpdate(Update update) {
-        SendMessage message = new SendMessage()
-                .setText("Я получил сообщение!")
-                .setChatId(update.getMessage().getChatId());
+        if (update.hasMessage()){
 
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+            // начало игры
+            if (update.getMessage().getText().toLowerCase().startsWith("я никогда не")
+                    || update.getMessage().getText().toLowerCase().startsWith("я ни разу не")){
+                long chatId = update.getMessage().getChatId();
+                String question = update.getMessage().getText();
+                Message resultMessage = new Message();
+
+                SendMessage msg = new SendMessage()
+                        .setChatId(chatId)
+                        .setText("Новый вопрос - \"" + question + "\"");
+
+                InlineKeyboardMarkup ikm = new InlineKeyboardMarkup();
+                List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+                List<InlineKeyboardButton> kbRow = new ArrayList<>();
+                kbRow.add(new InlineKeyboardButton().setCallbackData("never").setText("Никогда"));
+                kbRow.add(new InlineKeyboardButton().setCallbackData("ever").setText("Бывало"));
+                keyboard.add(kbRow);
+                ikm.setKeyboard(keyboard);
+                msg.setReplyMarkup(ikm);
+
+                try {
+                    resultMessage = execute(msg);
+                    int i = 1;
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+
+                startGame(chatId, resultMessage.getMessageId(), question);
+            }
+
+            // закончить игру
+            if (update.getMessage().getText().toLowerCase().startsWith("стоп игра")){
+                long chatId = update.getMessage().getChatId();
+
+                SendMessage msg = new SendMessage()
+                        .setChatId(chatId)
+                        .setText("Игра закончена!");
+
+                try {
+                    execute(msg);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+
+                endGame(chatId);
+            }
         }
     }
 
@@ -38,7 +106,7 @@ public class NeverEverBot extends TelegramLongPollingCommandBot {
     }
 
     private void registerCommands() {
-        BotCommand newQuestionCommand = new BotCommand("never","Я никогда не...") {
+        /*BotCommand newQuestionCommand = new BotCommand("never","Я никогда не...") {
             @Override
             public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
                 StringBuilder sb = new StringBuilder();
@@ -56,9 +124,23 @@ public class NeverEverBot extends TelegramLongPollingCommandBot {
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
+
+                startGame(chat.getId(), sb.toString().trim());
             }
         };
         register(newQuestionCommand);
+        */
+    }
+
+    private void startGame(long chatId, Integer messageId, String question){
+        Game gameToStart = new Game(this, chatId, messageId);
+        games.put(chatId, gameToStart);
+        gameToStart.startGame(question);
+    }
+
+    private void endGame(long chatId){
+        Game gameToEnd = games.remove(chatId);
+        gameToEnd.endGame();
     }
 
 }
--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,34 +1,61 @@
 package Game;
 
 import Bot.NeverEverBot;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-public class Game {
+public class Game extends Thread{
     private boolean active = false;
     private String question = "";
-    private static NeverEverBot bot;
+    private NeverEverBot bot;
+    private long chatId;
+    private int counter;
+    private Integer headerMessageId;
 
-    public static void main(String[] args) {
-        ApiContextInitializer.init();
+    public Game(NeverEverBot bot, long chatId, Integer headerMessageId) {
+        this.bot = bot;
+        this.chatId = chatId;
+        this.headerMessageId = headerMessageId;
 
-        TelegramBotsApi botsApi = new TelegramBotsApi();
-
-        bot = new NeverEverBot();
-
-        try {
-            botsApi.registerBot(bot);
-        } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
-        }
-
-
+        this.start();
     }
 
     public void startGame(String q) {
         question = q;
+        active = true;
     }
 
+    public void endGame(){
+        //this.interrupt(); // ?????????
+        active = false;
+    }
 
+    public void run() {
+        // main work here
+        // check updates, calc scores(?), countdown
+        do {
+            // =============================================================== DEBUG
+            EditMessageText msg = new EditMessageText()
+                    .setChatId(chatId)
+                    .setMessageId(headerMessageId)
+                    .setText(String.valueOf(counter++));
+            /*SendMessage msg = new SendMessage()
+                    .setChatId(chatId)
+                    .setText(String.valueOf(counter++));*/
+            try {
+                bot.execute(msg);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+            // =============================================================== DEBUG
+
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }while (active);
+        this.interrupt();
+    }
 }
